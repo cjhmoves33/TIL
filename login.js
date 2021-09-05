0e01a08bca660942ed94469262c2f4a23e7b708b
cjhmoves33 <cjhmoves33@gmail.com>
2021-09-05 23:46:55 +0900

    Make greeter

---
+++ login.js
@@ -0,0 +1,28 @@
+const login_box = document.querySelector('#login-form');
+const greeting = document.querySelector('#greeting');
+const owner = window.localStorage.getItem('ownerName');
+
+const HIDDEN_CLASS = 'hidden';
+const OWNER_NAME_KEY = 'ownerName';
+
+
+const greeter =function ( owner ) {
+    greeting.classList.remove( HIDDEN_CLASS );
+    greeting.innerText = `Hello, ${ owner }`;
+}
+
+const loginSubmit = function ( event ) {
+    const input = login_box[0].value;
+    event.preventDefault();
+    
+    window.localStorage.setItem( OWNER_NAME_KEY, input );
+    greeter( input );
+}
+
+if ( !owner ) {
+    login_box.classList.remove( HIDDEN_CLASS );
+    login_box.addEventListener ( 'submit', loginSubmit );
+} else {
+    greeter( owner );
+}
+
