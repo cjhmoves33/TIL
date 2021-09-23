ac338c6a7b9822ecac7140a38026dfcefad6951c
cjhmoves33 <cjhmoves33@gmail.com>
2021-09-23 23:52:17 +0900

    new simple

---
+++ mainTap.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './mainTap.scss';
+
+const MainTap = () => {
+  return (
+    <div id="grid">
+      <main className="main-back">
+      </main>
+      <footer className="buttons-back">
+      </footer>
+    </div>
+  )
+}
+
+export default MainTap;
