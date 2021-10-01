08b9c62b6ef89c35b573633fad35d0de4f8fdaca
cjhmoves33 <cjhmoves33@gmail.com>
2021-10-01 23:12:30 +0900

    sign up form

--- front/pages/signup.jsx
+++ front/pages/signup.jsx
@@ -4,18 +4,39 @@
 // next.js가 컴포넌트와 url을 1대1 매핑을 해줌.
 // next.js는 프론트서버를 가지고 있기 때문에 라우팅, SSR을 자동으로 해준다.
 
-// import React from 'react';
+import { useCallback, useMemo } from 'react';
+import { Form, Input } from 'antd';
+
 import AppLayout from '../components/AppLayout';
 import Head from 'next/head';
 
 const SignUp = () => {
+  const onSignUp = useCallback( () => {
+
+  }, []); 
+
+  const formInitialValue = useMemo( () => ({
+    remember: true,
+  }), [])
+
+  const signUpRules = useCallback( (value) => ({
+    require: true,
+    message: `${value}를 입력하세요.`
+  }), [])
+
   return (
     <>
       <Head>
         <title>sign up || node bird</title>
       </Head>
       <AppLayout>
-        <div>Sign Up Page</div>
+        <Form onFinish={onSignUp} initialValues={formInitialValue}>
+          <Form.Item name='username' rules={signUpRules('아이디')} >
+            <Input></Input>
+          </Form.Item>
+          <Form.Item name='password' rules={signUpRules('패스워드')} >
+          </Form.Item>
+        </Form>
       </AppLayout>
     </>
   );
