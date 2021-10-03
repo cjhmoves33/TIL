6380d8f7281910d93198a375a525372284b206e7
cjhmoves33 <cjhmoves33@gmail.com>
2021-10-03 22:00:12 +0900

    css최적화

--- front/components/FollowList.jsx
+++ front/components/FollowList.jsx
@@ -1,26 +1,33 @@
+import { useMemo } from 'react'
 import PropTypes from 'prop-types';
-import { List, Button, Card } from 'antd';
+import { List, Button, Card, Divider } from 'antd';
 import { DeleteOutlined } from '@ant-design/icons';
 
 const FollowList = ({ header, data }) => {
+  const listStyle = useMemo( () => ({ padding: '0 1rem 1rem 1rem' , marginBottom: '2rem'}), []);
+  const gridStyle = useMemo( () => ({ gutter: 8, xs: 3, sm:2 ,md: 3, lg: 3, xl: 3}), []);
+  const loadStyle = useMemo( () => ({ textAlign: 'center' }), []);
+  const cardStyle = useMemo( () => ({ marginTop: '1rem', textAlign: 'center' }), []);
+  const dividerStyle = useMemo( () => ({ marginTop: '1rem', marginBottom: 0 }), []);
+  
+  const cardAction = useMemo( () => ([<DeleteOutlined key='remove' />]), []);
+
   return (
-    <List
-    style={{ padding: (0, '1rem'), marginBottom: '2rem' }}
-    grid={{ gutter: 8, xs: 3, sm:2 ,md: 4}}
-    // size='small'
-    // bordered
-    header={<div>{header}</div>}
-    loadMore={<div style={{ textAlign: 'center' }}><Button>더 보기</Button></div>}
-    dataSource={data}
-    renderItem={ item => (
-      <List.Item>
-        <Card actions={[<DeleteOutlined key='stop' />]} style={{ marginTop: '1rem', textAlign: 'center' }} >
-          <Card.Meta title={item.nickname} />
-        </Card>
-      </List.Item>
-    )}
-    >
-    </List>
+    <>
+      <Divider orientation='left' style={dividerStyle} >{header}</Divider>
+      <List
+      style={listStyle}
+      grid={gridStyle}
+      loadMore={<div style={loadStyle}><Button>더 보기</Button></div>}
+      dataSource={data}
+      renderItem={ item => (
+        <List.Item>
+          <Card actions={cardAction} style={cardStyle} >
+            <Card.Meta title={item.nickname} />
+          </Card>
+        </List.Item>
+      )} />
+      </>
   )
 };
 
