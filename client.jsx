a965e6355a1f444cc57203dfdf82dd9cd2baeecd
cjhmoves33 <cjhmoves33@gmail.com>
2021-10-14 19:29:19 +0900

    디렉토리 위치수정

--- client.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-
-import MainTap from './mainTap'
-
-ReactDOM.render(<BrowserRouter><MainTap /></BrowserRouter>, document.querySelector('#root'));
