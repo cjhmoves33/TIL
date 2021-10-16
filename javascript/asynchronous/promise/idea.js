ae6c65465488770b894122fc60cec0bc1f497826
cjhmoves33 <cjhmoves33@gmail.com>
2021-10-16 17:03:57 +0900

    promise 객체에 대한 코드 수정

--- javascript/asynchronous/promise/idea.js
+++ javascript/asynchronous/promise/idea.js
@@ -3,7 +3,10 @@
 // ? Promise는 then 체이닝 자체는 소스코드 실행시 알단 동기적으로 모두 실행 되는가?
 const promise1 = new Promise((resolve, reject) => {
   console.log('첫번째');
-  resolve(200);
+  // resolve(200);
+  setTimeout(() => {
+    resolve(200);
+  })
 })
 console.log(promise1);
 
@@ -27,12 +30,16 @@ const promise2 = new Promise((resolve, reject) => {
 console.log(promise2);
 setTimeout(() => console.log('NOW promise2: ', promise2), 1000);
 
-// * 코드를 실행 시켜보면 new Promise의 콜백은 동기적으로 실행되는 것을 확인 할 수 있다.(console.log를 실행하고 resolve까지 실행한다.)
-// * console.log(promise1)의 결과를 보면 이행 상태의 Promise객체를 찍고
+// * 코드를 실행 시켜보면 new Promise의 콜백은 동기적으로 실행되는 것을 확인 할 수 있다.
+// * 비동기 처리는 new Promise의 콜백 안에서 이루어지는데 지금 setTimeout으로 비동기 처리를 하는거처럼 가정한다.
+// * 비동기 처리가 끝나기전(resolve가 호출되기전엔) promise1은 대기상태의 프로미스 객체이고
+// * 비동기 처리가 끝나면 promise1은 (여기선)이행상태인 프로미스객체가 된다.
+// // * console.log(promise1)의 결과를 보면 이행 상태의 Promise객체를 찍고(비동기처리없이 바로 resolve를 호출한경우)
 // * console.log(promise2)의 결과를 보면 대기 상태의 Promise객체를 찍는것을 확인할 수 있다.
 // * promise2의 경우 .then으로 메서드 체이닝을 한것을 알 수 있는데 이는 promise2가 단순히 new Promise로 만든 프로미스 객체가 아닌
 // * 그 이행상태에 따른 다음 Promise객체의 .then의 호출 값을 보여줌을 뜻한다.
-// * then의 콜백은 비동기로 이뤄진다. 그치만 then의 호출은 동기적으로 이루어진다. console.log(promise2)가 대기상태의 promise2를 찍는 이유이다.
+// ? promise2 첫 프로미스 객체 생성의 시기에는 이행된 상태의 프로미스객체이다. 그걸 then이 받은 상태
+// * then의 콜백은 비동기로 이뤄진다. 그치만 then의 호출은 (현재 코드 상에서는)동기적으로 이루어진다. console.log(promise2)가 대기상태의 promise2를 찍는 이유이다.
 // * setTimeout으로 console.log의 호출을 promise의 처리 뒤로 보낸 뒤 결과를 확인하면 이행 상태의 프로미스 객체를 찍는 것을 확인할 수 있다.
 
 console.log('----------------------------------------')
@@ -83,3 +90,16 @@ console.log('-------------------------- 여기가 동기 처리의 마지막 부
 // ? 이 순서대로 작업이 이루어 지기때문에 논 블로킹이 자연스럽게 이루어지는거같다. 그리고 프로미스의 비동기 실행 순서가 콘솔의 결과처럼 나오는 듯하다.
 
 // ? 여기서 한 예시는 서버와의 연동이 없기 때문에 실제 서버 요청을 해야하는 경우 어떤 식으로 사용할 수 있을지에 대해서도 생각해 봐야할거같다.
+
+
+
+// ! 수정사항
+new Promise((resolve, reject) => {
+  resolve();
+})
+new Promise((resolve, reject) => {
+  setTimeout(()=>resolve(), 2000);
+})
+// 이런 형태의 프로미스는 사실 비동기 처리의 예시는 아니라는 생각이든다.
+// 프로미스의 콜백 안에서 비동기 처리후 resolve를 호출 하게끔 구현을 해야 비동기 처리의 예시로서 사용 가능한거같다.
+// 비동기 처리를 안하고 바로 resolve를 호출한 형태는 사실 이행된 상태의 프로미스 객체를 뜻 함과 다를 바 없기 때문이다.
