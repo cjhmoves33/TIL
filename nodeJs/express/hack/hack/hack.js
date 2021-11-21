a054e38f44dee60de2b1a647559a33b471884298
cjhmoves33 <cjhmoves33@gmail.com>
2021-11-21 14:26:37 +0900

    hack 수정

--- nodeJs/express/hack/hack/hack.js
+++ nodeJs/express/hack/hack/hack.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const cookie = require('cookie-parser');
+const axios = require('axios');
 
 const app = express();
 
 app.use(cookie());
 
 app.get('/', (req, res) => {
-  console.log(req.cookies['connect.sid'])
+  // console.log(req.cookies['connect.sid'])
+
+  axios.get('http://localhost:3001/info')
+  .then(res => {
+    console.log(res.data);
+  })
+  .catch(err => console.error(err));
+
   res.redirect('http://localhost:3001');
   res.end();
   // * 다른 포트에서 세션값을 탈취 할 수 있음. 지금은 똑같은 localhost 도메인을 사용 중이여서 app.js에서 쿠키설정에 same site를 설정해줘도 전부 탈취가능한것임.
 })
+
 
 app.listen(3002, () => {
   console.log('hack 서버 3002포트.')
--- nodeJs/express/hack/service/app.js
+++ nodeJs/express/hack/service/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const session = require('express-session');
-const fs = require('fs').promises;
 const path = require('path');
+const cors = require('cors');
+const { User } = require('./models')
 
 const app = express();
+app.use(cors({
+  origin: 'http://localhost:3001'
+}))
 
 app.use(session({
   secret: 'keyboard cat',
@@ -18,13 +22,28 @@ app.use(session({
 
 app.get('/', (req, res) => {
   if(!req.session.test) {
-    req.session.test = 'information'
+    req.session.test = 'bok'
   }
   res.set({
     'Content-Type': 'text/html'
   })
   res.sendFile(path.join(__dirname, 'app.html'));
 });
+
+app.get('/info', async (req, res) => {
+  console.log(req.sessionID);
+  console.log(req.originalUrl)
+  if(req.sessionID) {
+    const result = await User.findOne({
+      where: {
+        username: 'bok'
+      }
+    })
+    res.send(result);
+  } else {
+    throw new Error('??');
+  }
+})
 
 app.listen(3001, () => {
   console.log('app 서버 3001포트.')
