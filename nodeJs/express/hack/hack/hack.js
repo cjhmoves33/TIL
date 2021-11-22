9d95f71f1ff34feb39789d5cd0218d03ab7fa3b0
cjhmoves33 <cjhmoves33@gmail.com>
2021-11-22 21:57:19 +0900

    조금 수정

--- nodeJs/express/hack/hack/hack.js
+++ nodeJs/express/hack/hack/hack.js
@@ -1,25 +1,31 @@
 const express = require('express');
 const cookie = require('cookie-parser');
 const axios = require('axios');
+const path = require('path');
 
 const app = express();
 
 app.use(cookie());
 
 app.get('/', (req, res) => {
-  // console.log(req.cookies['connect.sid'])
+  // console.log(req.cookies['connect.sid']);
 
-  axios.get('http://localhost:3001/info')
-  .then(res => {
-    console.log(res.data);
+  // axios.get('http://localhost:3001/info')
+  // .then(res => {
+  //   console.log(res.data);
+  // })
+  // .catch(err => console.error(err));
+
+  // res.redirect('http://localhost:3001');
+  // res.end();
+
+  res.set({
+    'Content-Type': 'text/html'
   })
-  .catch(err => console.error(err));
-
-  res.redirect('http://localhost:3001');
-  res.end();
+  res.sendFile(path.join(__dirname, 'app.html'));
+  // res.end();
   // * 다른 포트에서 세션값을 탈취 할 수 있음. 지금은 똑같은 localhost 도메인을 사용 중이여서 app.js에서 쿠키설정에 same site를 설정해줘도 전부 탈취가능한것임.
 })
-
 
 app.listen(3002, () => {
   console.log('hack 서버 3002포트.')
--- nodeJs/express/hack/service/app.js
+++ nodeJs/express/hack/service/app.js
@@ -1,50 +1,55 @@
 const express = require('express');
 const session = require('express-session');
 const path = require('path');
+
 const cors = require('cors');
 const { User } = require('./models')
 
 const app = express();
+
 app.use(cors({
-  origin: 'http://localhost:3001'
+  origin: 'http://127.0.0.1'
 }))
 
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    maxAge: 60000,
-    httpOnly: true,
-    sameSite: 'lax'
-  }
-}))
 
 app.get('/', (req, res) => {
-  if(!req.session.test) {
-    req.session.test = 'bok'
-  }
-  res.set({
-    'Content-Type': 'text/html'
+  // if(!req.session.test) {
+  //   req.session.test = 'bok'
+  // }
+  // res.set({
+    //   'Content-Type': 'text/html'
+    // })
+    // res.sendFile(path.join(__dirname, 'app.html'));
+    console.log('메인요청')
+    res.end();
+  });
+  
+  app.get('/info', async (req, res) => {
+    // console.log(req.sessionID);
+    // console.log(req.originalUrl);
+    if(req.sessionID) {
+      const result = await User.findOne({
+        where: {
+          username: 'bok'
+        }
+      })
+      res.send(result);
+    } else {
+      throw new Error('??');
+    }
   })
-  res.sendFile(path.join(__dirname, 'app.html'));
-});
+  
+  app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 60000,
+      httpOnly: true,
+      sameSite: 'lax'
+    }
+  }))
 
-app.get('/info', async (req, res) => {
-  console.log(req.sessionID);
-  console.log(req.originalUrl)
-  if(req.sessionID) {
-    const result = await User.findOne({
-      where: {
-        username: 'bok'
-      }
-    })
-    res.send(result);
-  } else {
-    throw new Error('??');
-  }
-})
-
-app.listen(3001, () => {
-  console.log('app 서버 3001포트.')
-});
+  app.listen(3001, () => {
+    console.log('app 서버 3001포트.')
+  });
